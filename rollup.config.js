bd907d3fa2f39fdc7c96eb1437be031df57a5c1c
Martin Acosta <tinchoz49@gmail.com>
2017-12-22 17:56:40 -0300

    added production build

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 const commonjs = require('rollup-plugin-commonjs');
 const resolve = require('rollup-plugin-node-resolve');
 const pkg = require('./package.json');
+const babel = require('rollup-plugin-babel');
 
 export default {
   input: 'src/index.js',
@@ -10,5 +11,11 @@ export default {
     sourcemap: true
   },
   external: ['mobx', 'mobx-state-tree'],
-  plugins: [resolve(), commonjs()]
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
 };
